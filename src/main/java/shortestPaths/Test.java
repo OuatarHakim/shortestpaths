c967b0dedf6048b787f9cdb8a0fd4d451e17003b
Hakim ouatar <hakimouatar6@gmail.com>
2022-11-19 08:38:11 +0100

    generator random graph

--- src/main/java/shortestPaths/Test.java
+++ src/main/java/shortestPaths/Test.java
@@ -1,4 +1,29 @@
 package shortestPaths;
 
+import org.graphstream.algorithm.generator.Generator;
+import org.graphstream.algorithm.generator.RandomGenerator;
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
+
 public class Test {
+    public static void main(String[] args) {
+
+        Graph g = genereRandomGraph(100, 4.0);
+    }
+
+    private static Graph genereRandomGraph(int n, double averageDegree) {
+        Graph graph = new SingleGraph("Random graphe");
+        RandomGenerator gen = new RandomGenerator(averageDegree);
+        gen.setDirectedEdges(true, true);
+        gen.addSink(graph);
+        gen.addEdgeAttribute("Poids");
+        gen.begin();
+        for (int i = 0; i < n; i++)
+            gen.nextEvents();
+        gen.end();
+
+        graph.edges().forEach(e -> e.setAttribute("Poids", (double) (Math.random() * 20) + 1));
+        return graph;
+    }
 }
