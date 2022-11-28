772fbe9a15a66b0cc7723676370a02539cc82598
Hakim ouatar <hakimouatar6@gmail.com>
2022-11-28 13:36:32 +0100

    update file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# shortestPaths
+# TP 3 Plus court chemain
+
+### Introduction :
+Dans ce tp , on  doit impléementer une version naiver de l'algorithme Dijkstra
+,afin de tester son efficacité on doit procéder à une campagne de test sur cet algorithme et le comparer à des tests effectués sur l’algorithme Dijkstra de GraphStream .
+Au premier temps on générera un graphe à l’aide de générateur de GraphStream , on lance les deux algorithmes et on mesure leur temps d'exécution sur des déférents taille  et degré de graphe .Et ensuite on répète les comparaison sur des graphes différents..
+
 
--- src/main/java/shortestPaths/Test.java
+++ src/main/java/shortestPaths/Test.java
@@ -1,15 +1,42 @@
 package shortestPaths;
 
+import org.graphstream.algorithm.Dijkstra;
 import org.graphstream.algorithm.generator.Generator;
 import org.graphstream.algorithm.generator.RandomGenerator;
 import org.graphstream.graph.Edge;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.stream.GraphParseException;
 
+import java.io.IOException;
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, GraphParseException {
+        //Naive dijkstra
+        Graph g = genereRandomGraph(4, 2.0);
 
-        Graph g = genereRandomGraph(100, 4.0);
+
+
+        System.out.println("Dijkstra NAIVE : ");
+
+        long temps = System.currentTimeMillis();
+        NaiveDijkstra naiveDijkstra = new NaiveDijkstra(g,g.getNode(0));
+
+        naiveDijkstra.computeDijkstra();
+        System.out.println(System.currentTimeMillis() - temps + " ms pour " + g.getNodeCount() + " sommet(s)");
+        //dijkstra graphStream
+        System.setProperty("org.graphstream.ui", "swing");
+        g.setAttribute("ui.quality");
+        g.setAttribute("ui.antialias");
+        g.setAttribute("ui.stylesheet" ,"url('data/style.css')");
+
+        g.nodes().forEach(n ->{n.setAttribute("ui.label", n.getId());
+
+      });
+        g.edges().forEach(e -> e.setAttribute("ui.label" ,e.getAttribute("Poids")));
+        g.display();
+
+
+
     }
 
     private static Graph genereRandomGraph(int n, double averageDegree) {
@@ -23,7 +50,8 @@ public class Test {
             gen.nextEvents();
         gen.end();
 
-        graph.edges().forEach(e -> e.setAttribute("Poids", (double) (Math.random() * 20) + 1));
+        graph.edges().forEach(e -> e.setAttribute("Poids", (double) Math.abs(Math.random() * 20) + 1));
+
         return graph;
     }
 }
