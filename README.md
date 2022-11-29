3b4118c12382b7494654100aa818bc9e41cf21c2
Hakim ouatar <hakimouatar6@gmail.com>
2022-11-29 19:25:48 +0100

    create simple random graphe with different n and degre

--- README.md
+++ README.md
@@ -4,7 +4,62 @@
 Dans ce tp , on  doit impléementer une version naiver de l'algorithme Dijkstra
 ,afin de tester son efficacité on doit procéder à une campagne de test sur cet algorithme et le comparer à des tests effectués sur l’algorithme Dijkstra de GraphStream .
 Au premier temps on générera un graphe à l’aide de générateur de GraphStream , on lance les deux algorithmes et on mesure leur temps d'exécution sur des déférents taille  et degré de graphe .Et ensuite on répète les comparaison sur des graphes différents..
+### Génération des graphes simples en utilisant random graph genrator
+![Generation de graphes simples  ](data/g2.png)   
 
+![Generation de graphes simples  ](data/g3.png)
+###  Générateur GraphStream  :
+### Générateur de graphe aléatoire :
+
+GraphStream propose un générateur de graphe aléatoire , On crée un simple graphe et on rajoute  à l’aide de ce générateur
+des sommets en respectant le degré moyen passé en paramètre .
+```java
+    private static Graph genereRandomGraph(int n, double averageDegree) {
+
+                Graph graph = new SingleGraph("Random graphe");
+                RandomGenerator gen = new RandomGenerator(averageDegree);
+                gen.setDirectedEdges(true, true);
+                gen.addSink(graph);
+                gen.addEdgeAttribute("Poids");
+                gen.begin();
+                for (int i = 0; i < n; i++)
+                    gen.nextEvents();
+                gen.end();
+        
+                graph.edges().forEach(e -> e.setAttribute("Poids", (double) Math.abs(Math.random() * 20) + 1));
+        
+                return graph;
+```
+
+On a crée un RandomGenerator en lui passant un degré moyen , on lui rajoutant une attribut poids sur les
+arretes et on on attribute des valeurs sur ces poids .
+
+### Algorithme Dijkstra 
+
+```java
+Procédure Dijkstra(G,s)
+v.dist ← ∞
+v.parent ← null pour v ∈ V
+s.dist ← 0
+f.init()
+f.add(s, 0)
+TantQue non f.empty() Faire
+u ← f.extractMin()
+Pour (u, v) ∈ E Faire
+Si v.dist > u.dist + wuv Alors
+v.dist ← u.dist + wuv
+v.parent ← u
+f.add(v, v.dist)
+FinSi
+FinPour
+FinTantQue
+FinProcédure
+```
+
+
+
+
+### Test 
 ![Test algorithme DijkstraNaive ](data/2graph.png)   
 
 
--- src/main/java/shortestPaths/Comparaison.java
+++ src/main/java/shortestPaths/Comparaison.java
@@ -1,36 +1,42 @@
 package shortestPaths;
 
 import org.graphstream.algorithm.Dijkstra;
-import org.graphstream.algorithm.generator.Generator;
 import org.graphstream.algorithm.generator.RandomGenerator;
-import org.graphstream.graph.Edge;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.SingleGraph;
 import org.graphstream.stream.GraphParseException;
 
 import java.io.IOException;
 public class Comparaison {
-    public static void main(String[] args) throws IOException, GraphParseException {
+    public static void main(String[] args)  {
+        //Essayez-le avec des exemples simples en prenant soin de générer des poids aléatoires sur les arêtes.
+
+        //createSimpleGraphe(5,2.0);
+
         //Naive dijkstra
-        Graph g = genereRandomGraph(4, 2.0);
+
+        Graph g = genereRandomGraph(500, 2.0);
         System.out.println("Dijkstra NAIVE : ");
 
         long temps = System.currentTimeMillis();
         NaiveDijkstra naiveDijkstra = new NaiveDijkstra(g,g.getNode(0));
-
         naiveDijkstra.computeDijkstra();
         System.out.println(System.currentTimeMillis() - temps + " ms pour " + g.getNodeCount() + " sommet(s)");
-        //dijkstra graphStream
-        System.setProperty("org.graphstream.ui", "swing");
-        g.setAttribute("ui.quality");
-        g.setAttribute("ui.antialias");
-        g.setAttribute("ui.stylesheet" ,"url('data/style.css')");
 
-        g.nodes().forEach(n ->{n.setAttribute("ui.label", n.getId());
+        System.out.println("Dijkstra graphStream : ");
+        temps = System.currentTimeMillis();
+        Dijkstra dijkstra = new Dijkstra(Dijkstra.Element.EDGE, null, "length");
 
-        });
-        g.edges().forEach(e -> e.setAttribute("ui.label" ,e.getAttribute("Poids")));
-        g.display();
+        // Compute the shortest paths in g from A to all nodes
+        dijkstra.init(g);
+        dijkstra.setSource(g.getNode(0));
+        dijkstra.compute();
+        System.out.println(System.currentTimeMillis() - temps + " ms pour " + g.getNodeCount() + " sommet(s)");
+
+
+
+
+
 
 
 
@@ -51,5 +57,20 @@ public class Comparaison {
 
         return graph;
     }
-}
+
+    private static void createSimpleGraphe(int nombreNodes , double averageDegree){
+        Graph g = genereRandomGraph(nombreNodes, averageDegree);
+
+        System.setProperty("org.graphstream.ui", "swing");
+        g.setAttribute("ui.quality");
+        g.setAttribute("ui.antialias");
+        g.setAttribute("ui.stylesheet" ,"url('data/style.css')");
+        g.nodes().forEach( n -> n.setAttribute("ui.label", n.getId()));
+        g.edges().forEach(e -> e.setAttribute("ui.label", " " + (Double) e.getNumber("Poids")));
+        g.display(true);
+    }
+
+    private static void compaison(){
+        
+    }
 }
